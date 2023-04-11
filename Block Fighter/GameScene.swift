1bc35665c60e36bb2b3f069f582f167bd1009b9d
Dhanush Tipparaju <dhanushtips@gmail.com>
2023-04-11 09:55:27 -0500

    set up variables for actions

--- Block Fighter/GameScene.swift
+++ Block Fighter/GameScene.swift
@@ -9,12 +9,18 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    var duck1 = SKSpriteNode()
+    var duck2 = SKSpriteNode()
+    var jump1 = SKSpriteNode()
+    var jump2 = SKSpriteNode()
+    var punch1 = SKSpriteNode()
+    var punch2 = SKSpriteNode()
     
     override func didMove(to view: SKView) {
         //this stuff happens once (when the app open)
-createBackground()
+        createBackground()
     }
-
+    
     func createBackground() {
         let space = SKTexture(imageNamed: "space2")
         for i in 0...1 {
@@ -29,4 +35,29 @@ createBackground()
             spaceBackground.run(moveForever)
         }
     }
+    
+    func createDuck1() {
+        
+    }
+    
+    func createDuck2() {
+        
+    }
+    
+    func createPunch1() {
+        
+    }
+    
+    func createPunch2() {
+        
+    }
+    
+    func createJump1() {
+        
+    }
+    
+    func createJump2() {
+        
+    }
 }
+
